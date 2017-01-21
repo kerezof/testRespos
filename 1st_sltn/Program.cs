416062aa151d98d42aca838611d5b4b6f9e5566b
Kerezof <Kerezof@KEREZOF-POWERPC>
2017-01-21 11:09:19 +0200

    Add project files.

---
+++ 1st_sltn/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1st_sltn
+{
+    class Program
+    {
+        static void Main()
+        {
+            var promenliva=int.Parse(Console.ReadLine());
+                      
+            Console.WriteLine(promenliva+12.54598);
+        }
+    }
+}
